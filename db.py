463bb8055bceee2c9aaff188e99fb439afedcaae
gosueep <eugin03@gmail.com>
2023-10-07 16:30:24 -0600

    move db

--- db.py
+++ db.py
@@ -9,13 +9,23 @@ def get_database():
  
    # Create the database for our example (we will use the same database throughout the tutorial
    return client['Peopler']
-  
-db = get_database()
-print(db)
-print(db["people"].find_one())
-for item in db["people"].find():
-    print(item)
 
 
 def db_create_user(user):
     db["people"].insert_one(user)
+    
+    
+
+if __name__ == "__main__":
+    db = get_database()
+    print(db)
+    print(db["people"].find_one())
+    for item in db["people"].find():
+        print(item)
+        
+    
+    db_create_user({
+        "test": "asda"
+    })
+    
+    
