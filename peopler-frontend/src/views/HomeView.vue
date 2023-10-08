157e5b79b0d985bf82f83cba68b7f60914327dc5
Rem Zhang <hello@itsrem.org>
2023-10-07 23:40:40 -0600

    (Mostly) finished onboarding page

--- peopler-frontend/src/views/HomeView.vue
+++ peopler-frontend/src/views/HomeView.vue
@@ -97,6 +97,10 @@ export default {
       }, 1250)
     },
     cont2 () {
+      if (!this.code_val.match(/[0-9]{6}/)) {
+        alert('Error: Invalid code entered.')
+        return
+      }
       this.showSpin = true
       setTimeout(() => {
         this.step = -1
